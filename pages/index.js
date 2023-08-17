588611235cfdca28561f1c2cb8d2d80121bb7716
Joseph Morales Silva <josephzx1@hotmail.com>
2023-08-16 21:42:59 -0600

    Rutas basicas

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+/**
+ * Pagina básica ya que no depende de algún contenedor
+ * Este crea una pagina partir de la estructura de carpetas de la app
+ */
+ const Home = () => {
+  return (
+    <div>
+        <h1>Hello World</h1>
+    </div>
+  )
+}
+export default Home
